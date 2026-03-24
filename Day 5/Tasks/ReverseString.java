33770a29f8ac3da4fe5d5e44e30827310741aa5c
VathsalyaB <vathsalyaabhi@gmail.com>
2026-03-24 23:11:12 +0530

    New file is added to new folder Day 5 under tasks folder inside Day 5

---
+++ Day 5/Tasks/ReverseString.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+class ReverseString {
+     public static void main(String[] args) {
+        char[] arr = {'h', 'e', 'l', 'l', 'o'};
+        reverseString(arr);
+
+        System.out.println(Arrays.toString(arr)); // [o, l, l, e, h]
+    }
+    public static void reverseString(char[] s) {
+        int left = 0, right = s.length - 1;
+
+        while (left < right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+}
