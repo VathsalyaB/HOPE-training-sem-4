b9f9776eced5b876d80421a430d6176f21128713
VathsalyaB <vathsalyaabhi@gmail.com>
2026-03-25 05:04:10 +0530

    new file added to Problems solved under Day 5

---
+++ Day 5/Problems solved/SBconcepts.java
@@ -0,0 +1,10 @@
+public class SBconcepts{
+    public static void main(String[] args){
+        
+        StringBuilder sb = new StringBuilder("Hello");
+        System.out.println("The value of the StringBuilder is: " + sb);
+        sb.append(" World");
+        System.out.println("The value of the StringBuilder after appending is: " + sb);
+       
+    }
+}
